f0014748d1b251d988c62fce9366f6ee87562a84
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-05 12:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function activeteSearch(){
+const searchDiv = document.querySelector('.search');
+searchDiv.classList.add('active');
+
+ const inputField = document.querySelector('.input');
+inputField.focus();
+}
